fee31ab7049f15ead56d18c1666834486827f6e9
Vu Van Tuu <vuvantuu.gl@gmail.com>
2023-12-25 13:16:50 +0700

    ant table

--- src/components/Users/UsersTable.tsx
+++ src/components/Users/UsersTable.tsx
@@ -1,41 +1,90 @@
-import { useEffect } from 'react';
-import '../../style/user.css'
+import { useEffect, useState } from 'react';
+// import '../../style/user.scss';
+import { Table } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
 
+interface Iusers{
+    id:string;
+    address:string;
+    age:number;
+    createdAt:Date;
+    email:string;
+    gender:string;
+    isVerify:boolean;
+    name:string;
+    role:string;
+    updatedAt:string;
+}
 const UsersTable = () => {
-
-    useEffect(()=>{
+   
+    const [listUsers, setListUsers] = useState([])
+    const getData = async ()=>{
+        const res = await fetch("http://localhost:8000/api/v1/auth/login", {
+            method: "POST",
+            body: JSON.stringify({
+                username: "admin@gmail.com",
+                password:"123456"
+            }),
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+              },
+        })
+        const data = await res.json();
+        console.log('data',data);
+    }
+    const token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjU4NjdmNDMzMDcwZDMzNTRhODc0NjFmIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3MDM0NzQ0MDEsImV4cCI6MTc4OTg3NDQwMX0.68AqqRyk7bcDb53RLe_rZ_KjYk8BqqIThfujI1Pu39s";
+    const getAllUser = async ()=>{
+        const res = await fetch("http://localhost:8000/api/v1/users/all", {
+            method: "GET",
+            
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${token}`,
+              },
+        })
+        const d = await res.json();
+        console.log('data user',d.data.result);
+        setListUsers(d.data.result);
         
+    }
+    useEffect(()=>{
+       
+        getData();
+        getAllUser();
+        console.log('list',listUsers);
     },[])
+    const conlums: ColumnsType<Iusers> = [
+        { 
+        title: 'Email',
+        dataIndex: 'email',
+        render :(value,record)=>{
+            return(
+                <a>{record.email}</a>
+            )
+        }
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name'
+        },
+        {
+            title: 'Role',
+            dataIndex: 'role'
+        }
+    ]
+
+   
     return (
         <div >
-            <div><h2>HTML Table</h2>
-            <table className="table table-striped">
-                    <thead>
-                    <tr>
-                        <th>Firstname</th>
-                        <th>Lastname</th>
-                        <th>Email</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>John</td>
-                        <td>Doe</td>
-                        <td>john@example.com</td>
-                    </tr>
-                    <tr>
-                        <td>Mary</td>
-                        <td>Moe</td>
-                        <td>mary@example.com</td>
-                    </tr>
-                    <tr>
-                        <td>July</td>
-                        <td>Dooley</td>
-                        <td>july@example.com</td>
-                    </tr>
-                    </tbody>
-                </table>
-                </div>
+            <h2>Users Table</h2>
+
+                <Table
+                columns={conlums}
+                dataSource={listUsers}
+                />
+               
+               
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
+  Link,
 } from "react-router-dom";
 import UsersPage from './screens/UsersPage.tsx';
+import { UserOutlined, HomeOutlined  } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
 
+const items: MenuProps['items'] = [
+  {
+    label: <Link to='/'>Home</Link>,
+    key: 'homes',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: <Link to='/users'>Manage Users</Link>,
+    key: 'users',
+    icon: <UserOutlined />,
+  },
+];
+const Headers: React.FC = () => {
+  const [current, setCurrent] = useState('homes');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return (<Menu
+     onClick={onClick}
+      selectedKeys={[current]} 
+      mode="horizontal" 
+      items={items} />
+      );
+};
+const LayoutAdmin =()=>{
+  return (
+    <>
+    <Headers/>
+    <div><Outlet/></div>
+    </>
+    
+  )
+}
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
+    element: <LayoutAdmin/>,
+    children:[
+      { index:true, element:<App/>},
+      {
+        path: "users",
+        element: <UsersPage/>,
+      },
+    ]
   },
-  {
-    path: "/users",
-    element: <UsersPage/>,
-  },
+ 
   {
     path: "/tracks",
     element: <div>manage tracks</div>,
